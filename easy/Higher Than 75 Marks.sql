f0863cfee6ce0e505e73341ef3200e5bf7812036
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-05-05 08:18:22 +0300

    higher 75 marks

---
+++ easy/Higher Than 75 Marks.sql
@@ -0,0 +1,23 @@
+/* LINK https://www.hackerrank.com/challenges/more-than-75-marks/problem?isFullScreen=true */
+
+
+/* 
+CONTEXT:
+Query the Name of any student in STUDENTS who scored higher than  Marks
+
+
+OUTPUT:
+Name 
+
+ORDER BY
+last three characters of each name.
+id ASC
+
+*/
+
+/* SOLUTION */
+
+SELECT NAME 
+FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY RIGHT(NAME, 3) ASC, ID ASC;
